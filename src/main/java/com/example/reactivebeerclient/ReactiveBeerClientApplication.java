73add0b5fd46a10f6d1abff78417bc686a68a347
olzhas <serikbayev97@gmail.com>
2022-12-22 09:26:42 +0600

    test

--- src/main/java/com/example/reactivebeerclient/ReactiveBeerClientApplication.java
+++ src/main/java/com/example/reactivebeerclient/ReactiveBeerClientApplication.java
@@ -1,7 +1,11 @@
 package com.example.reactivebeerclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 public class ReactiveBeerClientApplication {
@@ -9,5 +13,9 @@ public class ReactiveBeerClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveBeerClientApplication.class, args);
 	}
+
+	@Value("classpath:/schema.sql")
+	Resource resource;
+
 
 }
